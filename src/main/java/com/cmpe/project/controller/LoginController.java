c988128b71131ba0a64246ea480c88e542ae5321
Nishant Mendiratta <nishant.mendiratta89@gmail.com>
2014-12-10 15:08:19 -0800

    user login API + Authentication

--- src/main/java/com/cmpe/project/controller/LoginController.java
+++ src/main/java/com/cmpe/project/controller/LoginController.java
@@ -64,4 +64,79 @@ public class LoginController {
 		return RBean;
 	}
 
+	/**
+	 * @param user
+	 * @return
+	 * @throws NoSuchAlgorithmException
+	 */
+	/**
+	 * @param user
+	 * @return
+	 * @throws NoSuchAlgorithmException
+	 */
+	@RequestMapping(value = "/userlogin", method = RequestMethod.POST)
+	@ResponseBody
+	public Object loginuser(@RequestBody LoginTO user)
+			throws NoSuchAlgorithmException {
+		RegistrationBean RBean = new RegistrationBean();
+		String user_name = user.getUsername();
+		String password = user.getPassword();
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		md.update(password.getBytes());
+		byte[] bytes = md.digest();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < bytes.length; i++) {
+			sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16)
+					.substring(1));
+		}
+		System.out.println(sb.toString());
+
+		DBCollection dbcUser = SpringConfig.db
+				.getCollection("registrationBean");
+
+		
+		BasicDBObject query = null;
+		
+		query=new BasicDBObject("_id",user_name);
+		DBCursor cursor = dbcUser.find(query);
+		if(cursor.hasNext()) 
+		{
+			query = new BasicDBObject("_id", user_name).append("password",
+					sb.toString());
+			cursor=dbcUser.find(query);
+			if(cursor.hasNext())
+			{
+				System.out.println("welcome user");
+				httpSession.setAttribute(Constants.LOGGED_IN_USER, user_name);
+				return user;
+			}
+			else
+			{
+				System.out.println("incorrect password");
+				return "password failed";
+			}	
+		}
+		else
+		{
+			System.out.println("user does not exist. Please Sign up");
+			return "No User";
+		}
+		
+		/*query = new BasicDBObject("username", user_name).append("password",
+				sb.toString());
+		*/
+		//System.out.println("in post");
+		
+		
+		/*DBCursor cursor = dbcUser.find(query);
+		if (cursor.hasNext()) {
+			httpSession.setAttribute(Constants.LOGGED_IN_USER, user_name);
+			return user;
+		} else {
+			return "";
+		}*/
+
+	}
+
+
 }
