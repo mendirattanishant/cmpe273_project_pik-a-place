241e8d6995fdca7481f59d1ea1de3fa4f3a98787
raunaqmathur <raunaq.mathur22@gmail.com>
2014-12-10 14:41:24 -0800

    latest user photo display code

--- src/main/java/Database/SearchFriendsWithPlace.java
+++ src/main/java/Database/SearchFriendsWithPlace.java
@@ -53,7 +53,6 @@ public class SearchFriendsWithPlace {
 
 	}
 
-
 	public List<FilteredUserPhotoPlace> GetFriendListNPhotos(String parentId,
 			double latitude, double longitude) throws UnknownHostException,
 			JSONException {
@@ -130,8 +129,7 @@ public class SearchFriendsWithPlace {
 		}
 	}
 
-	
-		public void GetFriendPlaceList(String parentId, DB db) {
+	public void GetFriendPlaceList(String parentId, DB db) {
 
 		DBCollection dbcUser = db.getCollection("UserPlaces");
 
@@ -196,6 +194,91 @@ public class SearchFriendsWithPlace {
 		}
 	}
 
+	public void GetFriendPhoto(String parentId, DB db) throws JSONException {
 
+		DBCollection dbcUser = db.getCollection("UserPhotosPlaces");
+
+		BasicDBObject query = null;
+		// int counter = 0;
+		for (int i = 0; i < dataPlaceFiltered.size(); i++) {
+			// System.out.println(dataFriend.get(i));
+			// System.out.println("displaying for " + dataFriendFiltered.get(i)
+			// + "  " + dataPlaceFiltered.get(i));
+			query = new BasicDBObject("placeId", dataPlaceFiltered.get(i));
+			query.append("parentId", dataFriendFiltered.get(i));
+
+			DBCursor cursor = dbcUser.find(query);
+			DBObject dbotemp = null;
+			FilteredUserPhotoPlace fTemp = null;
+			JSONObject tempObj = null;
+			try {
+				while (cursor.hasNext()) {
+					dbotemp = cursor.next();
+					// System.out.println("pic is " +
+					// dbotemp.get("photoId").toString());
+					// dataPhotoFiltered.add(dataFriendFiltered.get(i));
+					// dataFriendPhoto.put(dataFriendFiltered.get(i),
+					// dbotemp.get("photoId").toString());
+					fTemp = new FilteredUserPhotoPlace();
+					fTemp.placeId = dataPlaceFiltered.get(i);
+					fTemp.photoId = dbotemp.get("photoId").toString()
+							.replace("[", "").replace("]", "")
+							.replace("\"", "").trim();
+					fTemp.parentId = dataFriendFiltered.get(i);
+					fTemp.friendName = dataFriendNameFiltered.get(i);
+
+					tempObj = dataLocationFiltered.get(i)
+							.getJSONObject("place").getJSONObject("location");
+					if (tempObj.has("zip"))
+						fTemp.zip = tempObj.getString("zip");
+					else
+						fTemp.zip = "";
+
+					if (tempObj.has("street"))
+						fTemp.street = tempObj.getString("street");
+					else
+						fTemp.street = "";
+
+					if (tempObj.has("state"))
+						fTemp.state = tempObj.getString("state");
+					else
+						fTemp.state = "";
+
+					if (tempObj.has("longitude"))
+						fTemp.longitude = tempObj.getString("longitude");
+					else
+						fTemp.longitude = "";
+
+					if (tempObj.has("latitude"))
+						fTemp.latitude = tempObj.getString("latitude");
+					else
+						fTemp.latitude = "";
+
+					if (tempObj.has("country"))
+						fTemp.country = tempObj.getString("country");
+					else
+						fTemp.country = "";
+
+					if (tempObj.has("city"))
+						fTemp.city = tempObj.getString("city");
+					else
+						fTemp.city = "";
+
+					if (dataLocationFiltered.get(i).getJSONObject("place")
+							.has("name"))
+						fTemp.name = dataLocationFiltered.get(i)
+								.getJSONObject("place").getString("name");
+					else
+						fTemp.name = "";
+
+					filteredData.add(fTemp);
+
+				}
+			} finally {
+				cursor.close();
+
+			}
+		}
+	}
 
 }
