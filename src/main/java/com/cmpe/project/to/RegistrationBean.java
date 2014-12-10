1032aab9f233059e8b92c3c9e058813bfc2bdd6b
aparnakansal <aparnakansal@gmail.com>
2014-12-09 20:04:25 -0800

    Uploaded Getter Setter for registration form

---
+++ src/main/java/com/cmpe/project/to/RegistrationBean.java
@@ -0,0 +1,48 @@
+package com.cmpe.project.to;
+
+import org.springframework.data.annotation.Id;
+
+public class RegistrationBean {
+
+	String firstname;
+	String lastname;
+	@Id
+	String username;
+	String password;
+
+	
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+
+}
