0697a210556bb026834abb9d5b41a81d28101ce8
Nishant Mendiratta <nishant.mendiratta89@gmail.com>
2014-12-10 15:30:36 -0800

    Changes made in Login Javascript

--- src/main/webapp/resources/js/Login.js
+++ src/main/webapp/resources/js/Login.js
@@ -1,90 +1,71 @@
-/**
- * 
- */
 function Login()
 {
 	
-      return user();
+      return username() && password() &&user();
       
       function username()
       {
-            var username=document.getElementById("username").value;
+    	  
+    	  var username=document.getElementById("username").value;
             document.getElementById("error1").innerHTML="";
-            var reg=/^[A-Za-z0-9\s]{4,20}$/;
             if(username==null || username=="")
             {
+            	alert("username cannot be blank");
             document.getElementById("error1").innerHTML="username can not be blank";
             document.getElementById("username").focus();
             return false;
             }
-            }   
-            return true;
-      }
-      
+            return true;	     
+      }      
       function password()
       {
+    	  
     	  var password=document.getElementById("password").value;
           document.getElementById("error2").innerHTML="";
-          var reg=/^[A-Za-z0-9\s]{6,30}$/;
           if(password==null || password=="")
           {
+        	  alert("password cannot be blank");
           document.getElementById("error2").innerHTML="Password cannot be blank";
           document.getElementById("password").focus();
           return false;
-
           }     
           return true;
       }
-      function user() {
-        		alert("1");
-        		var username=document.getElementById("username").value;
+      
+            function user() {
+            	var username=document.getElementById("username").value;
         	    var password=document.getElementById("password").value;
         	    
         	    
         	    var string={ 
         	    		"username" : username,"password" : password
         	    };
-        	    alert(string);
-
-        	         /* $.ajax({
-        	             type: "POST",
-        	             url: "http:/localhost:8080/LoginController1",
-        	             contentType: "application/json",
-        	             data:JSON.stringify(string),
-        	             dataType: "JSON",
-        	             success: function(data){        
-        	                alert(data);
-        	             },
-        	         error : function(jqXHR, textStatus, errorThrown){
-        	        	 alert(textStatus + errorThrown); 
-        	         }
-        	         });  */
         	    	jQuery.ajax({
         	        type: "POST",
         	        url: "http://localhost:8080/userlogin",
         	        data:JSON.stringify(string),
         	        contentType: "application/json; charset=utf-8",
-        	        dataType: "json",
+        	        dataType: "text",
         	        success: function (data, status, jqXHR) {
-        	             
-        	             alert(status);
-        	             window.location.href="/success"
+        	             alert("Welcome");
+        	             var abc=data;
+        	        	if(abc=="password failed")
+        	        		alert("password is incorrect")
+        	        		else
+        	        			if(abc=="No User")
+        	        				alert("user does not exist. Please Sign up")
+        	        				else
+        	        					window.location.href="/success"
         	        },
         	    
         	        error: function (jqXHR, status, textStatus) {            
         	             // error handler
         	        	 
-        	             alert("error " + status + " Text" + textStatus);
+        	             alert(textStatus);
         	             window.location.href="/Error"
         	        }
 
         	    });
 
         	}
-          
-
-
-
-
-
-
+}
